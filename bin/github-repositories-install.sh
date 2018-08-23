5f2bb1af5ab6c95ba597635282690d460079f636
Hugues Dubois <hugdubois@gmail.com>
2018-08-23 18:51:49 +0200

    Some improvements

---
+++ bin/github-repositories-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+GITHUB_PATH="$HOME/go/src/github.com"
+
+REPOS="gomeet/gomeet
+gomeet/go-proto-gomeetfaker
+gomeet/faker
+gomeet/elm-protobuf
+gomeet/gomeet-tools-markdown-server
+gomeet/gomeet-godoc
+gomeet/gomeet-builder
+gomeet/gomeet-go-swagger
+hugdubois/ah-svc-www
+"
+for repo in $REPOS; do
+	if [ -d "$GITHUB_PATH/$repo" ]; then
+		echo "[SKIP] - $repo derectory: $GITHUB_PATH/$repo exists"
+	else
+		mkdir -p $GITHUB_PATH/$repo
+		git clone https://github.com/$repo $GITHUB_PATH/$repo
+	fi;
+done
