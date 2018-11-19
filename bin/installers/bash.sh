e980d5821bad065b9dcb61e4d3769bc5dc8088ea
Hugues Dubois <hugdubois@gmail.com>
2018-11-19 15:48:27 +0100

    Some improvements

--- bin/installers/bash.sh
+++ bin/installers/bash.sh
@@ -6,6 +6,8 @@ DOTFILES_PATH=$(realpath $SCRIPT_PATH/../..)
 
 
 # bash shell
+[ -d "$HOME/.env" ] && rm -rf "$HOME/.env"
+ln -s "$DOTFILES_PATH/env" "$HOME/.env"
 [ -d "$HOME/.profile" ] && rm -rf "$HOME/.profile"
 ln -s "$DOTFILES_PATH/profile" "$HOME/.profile"
 [ -d "$HOME/.bash_aliases" ] && rm -rf "$HOME/.bash_aliases"
