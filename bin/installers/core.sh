9e6953af429673bf3427e747597256a2e3e59758
Hugues Dubois <hugdubois@gmail.com>
2019-04-15 15:12:13 +0200

    Some adjustement on installers

--- bin/installers/core.sh
+++ bin/installers/core.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-SCRIPT=$(readlink -f "$0")
+case $(uname -s) in
+    Linux*)     SCRIPT=$(readlink -f "$0") ;;
+    Darwin*)    SCRIPT=$(greadlink -f "$0") ;;
+    *)          echo "UNKNOW OS" && exit 1
+esac
 SCRIPT_PATH=$(dirname "$SCRIPT")
 DOTFILES_PATH=$(realpath $SCRIPT_PATH/../..)
-
 
 mkdir -p $HOME/.local/bin
 sudo mkdir -p /usr/local/bin
@@ -15,4 +18,7 @@ mkdir -p $HOME/bin
 ln -s "$DOTFILES_PATH/bin/ag-replace" "$HOME/bin/ag-replace"
 [ -f "$HOME/bin/tar-grep" ] && rm -rf $HOME/bin/tar-grep
 ln -s "$DOTFILES_PATH/bin/tar-grep" "$HOME/bin/tar-grep"
+
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
 
--- bin/installers/nvim.sh
+++ bin/installers/nvim.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-SCRIPT=$(readlink -f "$0")
+case $(uname -s) in
+    Linux*)     SCRIPT=$(readlink -f "$0") ;;
+    Darwin*)    SCRIPT=$(greadlink -f "$0") ;;
+    *)          echo "UNKNOW OS" && exit 1
+esac
 SCRIPT_PATH=$(dirname "$SCRIPT")
 DOTFILES_PATH=$(realpath $SCRIPT_PATH/../..)
 
