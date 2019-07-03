23b032e3a90cabab9af7598173f9821116322788
Hugues Dubois <hugdubois@gmail.com>
2019-07-03 14:24:22 +0200

    Added intellij config

---
+++ bin/installers/intellij.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+case $(uname -s) in
+	Linux*)     SCRIPT=$(readlink -f "$0") ;;
+	Darwin*)    SCRIPT=$(greadlink -f "$0") ;;
+	*)          echo "UNKNOW OS" && exit 1
+esac
+SCRIPT_PATH=$(dirname "$SCRIPT")
+DOTFILES_PATH=$(realpath $SCRIPT_PATH/../..)
+
+[ -d "$HOME/.ideavimrc" ] && rm -rf "$HOME/.ideavimrc"
+ln -s "$DOTFILES_PATH/ideavimrc" "$HOME/.ideavimrc"
+
