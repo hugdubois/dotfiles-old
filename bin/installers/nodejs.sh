ed40d0df36415a24485e511e37fa52236dc37cd9
Hugues Dubois <hugdubois@gmail.com>
2019-07-10 12:29:21 +0200

    Added TypeScript

--- bin/installers/nodejs.sh
+++ bin/installers/nodejs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 case $(uname -s) in
-    Linux*)     SCRIPT=$(readlink -f "$0") ;;
-    Darwin*)    SCRIPT=$(greadlink -f "$0") ;;
-    *)          echo "UNKNOW OS" && exit 1
+	Linux*)     SCRIPT=$(readlink -f "$0") ;;
+	Darwin*)    SCRIPT=$(greadlink -f "$0") ;;
+	*)          echo "UNKNOW OS" && exit 1
 esac
 SCRIPT_PATH=$(dirname "$SCRIPT")
 DOTFILES_PATH=$(realpath $SCRIPT_PATH/../..)
@@ -15,3 +15,7 @@ npm install -g neovim
 npm install -g gitbook-cli sass
 #npm install -g diagrams grpcc instant-markdown-d mermaid.cli
 npm install -g elchemy
+
+# typescript / react
+npm install -g typescript
+npm install -g create-react-app
